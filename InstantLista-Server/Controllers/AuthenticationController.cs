21b0d3817d5cf670f9982238612fa1539820da4c
David <daviddelaokeller@hotmail.com>
2023-02-05 22:05:27 -0400

    new server

---
+++ InstantLista-Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using InstantLista_Server.Models;
+
+namespace InstantLista_Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly ILogger<AuthenticationController> _logger;
+
+    public AuthenticationController(ILogger<AuthenticationController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("login",Name = "GetJWT")]
+    [AllowAnonymous]
+    public async Task GetJWT([FromBody] UserAuthenticationModel userAuthenticationModel)
+    {
+        
+    }
+}
+
